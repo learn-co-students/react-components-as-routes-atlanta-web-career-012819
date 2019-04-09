83791c5ba4a594983ab7d41ac6995eb52c82da7d
github username <github email address>
2019-04-09 10:30:42 -0400

    working app with working routes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,72 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Home from './home';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
+// const Home = () => {
+//     return (
+//         <div>
+//             <h1>Home!</h1>
+//         </div>
+//     );
+// };
+
+const About = () => {
+    return (
+        <div>
+            <h1>This is my about component!</h1>
+        </div>
+    );
 };
 
-ReactDOM.render(
-  <Home />,
-  document.getElementById('root')
+const Login = () => {
+    return (
+        <div>
+            <form>
+                <div>
+                    <input type="text" name="username" placeholder="Username" />
+                    <label htmlFor="username">Username</label>
+                </div>
+                <div>
+                    <input type="password" name="password" placeholder="Password" />
+                    <label htmlFor="password">Password</label>
+                </div>
+                <input type="submit" value="Login" />
+            </form>
+        </div>
+    );
+};
+
+
+ReactDOM.render((
+    <Router>
+        <React.Fragment>
+            <Route path="/" component={Home} />
+            {/* <Route exact path="/" render={Home} /> */}
+            <Route exact path="/about" render={About} />
+            <Route exact path="/login" render={Login} />
+        </React.Fragment>
+    </Router>),
+    document.getElementById('root')
 );
+
+
+// ReactDOM.render((
+//     <Router>
+//         <React.Fragment>
+//             {/* <Route path="/" render={Home} /> */}
+//             <Route exact path="/" render={Home} />
+//             <Route exact path="/about" render={About} />
+//             <Route exact path="/login" render={Login} />
+//         </React.Fragment>
+//     </Router>),
+//     document.getElementById('root')
+// );
+
+
+
+
+// ReactDOM.render(
+//   <Home />,
+//   document.getElementById('root')
+// );
